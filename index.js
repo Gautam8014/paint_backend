279e1474e646f5319cfaf180a9f5da0d7c16dcc5
Gautam8014 <flt.gautam@gmail.com>
2023-10-13 14:23:50 +0530

    update

--- index.js
+++ index.js
@@ -1,136 +1,85 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient } = require('mongodb');
-const {connection} = require("./congfig/db");
-const {userModel} = require("./models/User.model");
- 
+const mongoose = require("mongoose");
+const userModel = require("./models/User.model");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-const PORT = process.env.PORT || 3000
 
-const uri = process.env.mongoDBUrl;
-const client = new MongoClient(uri)
+const PORT = process.env.PORT || 8000;
 
-app.get("/",(req,res) =>{
-    res.send("Welcome to home");
-})
+const mongoURI = 'mongodb+srv://gautam:9507254304@cluster0.cfldg6s.mongodb.net/paint?retryWrites=true&w=majority';
 
-// for gallery
-app.get("/gallery", async(req, res) =>{
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB: ' + err);
+  });
 
+app.get("/", (req, res) => {
+  res.send("Welcome to home");
+});
 
-    try {
-
+app.get("/gallery", async (req, res) => {
+  try {
     const displayPaint = await userModel.find();
     res.send(displayPaint);
-        
-    } catch (error) {
-        console.log(error);
-        console.log("Error While loading Get Method");
-        
-    }
-    
-
-
-})
-
-
+  } catch (error) {
+    console.log("Error While loading Get Method", error);
+    res.status(500).json({ message: "Error while loading gallery" });
+  }
+});
 
 app.get("/gallery/:id", async (req, res) => {
-    try {
-      const productId = req.params.id; // Get the 'id' parameter from the URL
-      const product = await userModel.findOne({ _id: productId });
-  
-      if (!product) {
-        // Handle the case where the product with the given ID is not found
-        return res.status(404).json({ message: "Product not found" });
-      }
-  
-      res.json(product);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Error while loading product" });
+  try {
+    const productId = req.params.id;
+    const product = await userModel.findOne({ _id: productId });
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
     }
-  });
-  
 
+    res.json(product);
+  } catch (error) {
+    console.error("Error while loading product", error);
+    res.status(500).json({ message: "Error while loading product" });
+  }
+});
 
-
-app.post("/post", async(req, res) =>{
-    //  res.send("Welcome to post page");
-    // const payload = req.body;
-    // console.log(payload);
-
-
-    // try {
-
-    // const newPaint =  userModel(payload);
-    // await newPaint.save();
-
-    // res.send({Msg: "Post Successfully", newPaint});
-        
-    // } catch (error) {
-    //     console.log(error);
-    //     console.log("Error!!!! While Post Method");
-        
-    // }
+app.post("/post", async (req, res) => {
+  try {
     const payload = req.body;
     console.log(payload);
+    const newPaint = new userModel(payload);
+    await newPaint.save();
+    res.send({ Msg: "Post Successfully", newPaint });
+  } catch (error) {
+    console.error("Error while posting", error);
+    res.status(500).json({ message: "Error while posting" });
+  }
+});
 
-    const newNotes =new userModel(payload);
-    await newNotes.save();
-    res.send({Msg : "Created New Notes", newNotes});
-    
-})
-
-
-
-
-// app.delete("/delet", (req, res) =>{
-//     res.send("Welcome to delet");
-// })
 app.delete("/gallery/:notesId", async (req, res) => {
-    const notesId = req.params.notesId;
-
-    try {
-        const deletedNote = await userModel.findByIdAndRemove(notesId);
-
-        if (!deletedNote) {
-            return res.status(404).json({ message: "Note not found" });
-        }
-
-        res.json({ message: "Note deleted successfully" });
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({ message: "Internal server error" });
+  const notesId = req.params.notesId;
+  try {
+    const deletedNote = await userModel.findByIdAndRemove(notesId);
+    if (!deletedNote) {
+      return res.status(404).json({ message: "Note not found" });
     }
+    res.json({ message: "Note deleted successfully" });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
-
-// app.listen(PORT, async()=>{
-
-//    try {
-//     await connection()
-//     console.log("Connected to Mongo");
-    
-//    } catch (error) {
-//     console.log(error);
-//     console.log("Error!!!!! Not Connect to MongoDB");
-//    }
-
-
-
-    
-
-// });
-client.connect(err => {
-    if(err){ console.error(err); return false;}
-    // connection to mongo is successful, listen for requests
-    app.listen(PORT, () => {
-        console.log("listening for requests");
-    })
-});
-
-
